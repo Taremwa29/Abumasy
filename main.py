7570a278900c9a8dca83b4d364324bbc2645eb29
Taremwa29 <103855038+Taremwa29@users.noreply.github.com>
2024-01-03 12:13:43 +0300

    Updates to delete cache and manage storage

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 from app.init import app
+import os
+
+def clear_pycache(directory):
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if file.endswith(".pyc"):
+                file_path = os.path.join(root, file)
+                os.remove(file_path)
+                print(f"Deleted: {file_path}")
 
 if __name__ == "__main__":
+    application_directory = '/Abumasy'
+    clear_pycache(application_directory)
+    print("PyCache cleared successfully.")
     app.run(debug= True)
